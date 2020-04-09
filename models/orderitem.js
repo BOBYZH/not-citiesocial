74c098ed6a9d15f49d9de9b802217646df0dbe6d
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-04-10 01:00:53 +0800

    Define relationships between models

--- models/orderitem.js
+++ models/orderitem.js
@@ -1,8 +1,13 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const OrderItem = sequelize.define('OrderItem', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     OrderId: DataTypes.INTEGER,
-    PoductId: DataTypes.INTEGER,
+    ProductId: DataTypes.INTEGER,
     price: DataTypes.INTEGER,
     quantity: DataTypes.INTEGER,
     type: DataTypes.STRING
