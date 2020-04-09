5873cbedaac1f9d0b79405a5f93b6330995fce91
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-04-10 01:41:11 +0800

    Generate seeds for fake data, and install relative packages

---
+++ seeders/20200409170758-users-seed-file.js
@@ -0,0 +1,41 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      is_admin: true,
+      first_name: 'root',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      is_admin: false,
+      first_name: 'user1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      is_admin: false,
+      first_name: 'user2',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {})
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+}
