436150b97b0b64caf64f292b9ed6cbf63ff81ce0
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-04-16 01:35:45 +0800

    Add getIndex and more detailed annotations, and Arrange codes

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,18 +2,40 @@ const db = require('../models')
 const Product = db.Product
 // const Category = db.Category
 // const User = db.User
-const PAGE_LIMIT = 3
-const PAGE_OFFSET = 0
+
+// 設定顯示數量
+let PAGE_LIMIT, PAGE_OFFSET
 
 const productController = {
-  redirectInvalidUrl: (req, res) => { // 防止亂打網址出現404錯誤
+  // 防止亂打網址出現404錯誤
+  redirectInvalidUrl: (req, res) => {
     res.redirect('/')
   },
 
+  // 後台管理
   getProducts: (req, res) => {
-    Product.findAndCountAll({ offset: PAGE_OFFSET, limit: PAGE_LIMIT }).then(products => {
-      return res.render('products', JSON.parse(JSON.stringify({
+    Product.findAndCountAll().then(products => {
+      return res.render('admin/products', JSON.parse(JSON.stringify({
         products
+      })))
+    })
+  },
+
+  // 前台瀏覽
+  getIndex: (req, res) => {
+    PAGE_LIMIT = 5
+    PAGE_OFFSET = 0
+    Product.findAndCountAll({ offset: PAGE_OFFSET, limit: PAGE_LIMIT }).then(products => {
+      const Products = products.rows
+        .map(product => (
+          {
+            ...product.dataValues,
+            name: product.dataValues.name !== null ? (product.dataValues.name.length > 18 ? (product.dataValues.name.substring(0, 18) + '...') : product.dataValues.name) : ''
+          }
+        ))
+      console.log(products.rows[0].name)
+      return res.render('index', JSON.parse(JSON.stringify({
+        products: Products
       })))
     })
   },
@@ -33,10 +55,10 @@ const productController = {
         })))
       }
     })
-    // .catch((product) => {
-    //   req.flash('error_messages', '系統錯誤！')
-    //   res.redirect('/')
-    // })
+      .catch((product) => {
+        req.flash('error_messages', '系統錯誤！')
+        res.redirect('/')
+      })
   }
 }
 
