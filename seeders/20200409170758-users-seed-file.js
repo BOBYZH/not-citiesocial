3ad6f6384a34dd9bb09a757b119a64c918765940
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-04-23 02:08:56 +0800

    Add customer, and more shop admin accounts

--- seeders/20200409170758-users-seed-file.js
+++ seeders/20200409170758-users-seed-file.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs')
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
+      // （未來設為系統管理員帳號，先當一般店家帳號）
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
@@ -14,6 +15,7 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
+      // 店家帳號1
       email: 'user1@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
@@ -22,11 +24,21 @@ module.exports = {
       lastName: 'Customer',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
+      }, {
+      // 店家帳號2
       email: 'user2@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       shopName: 'Truth',
+      firstName: 'User2',
+      lastName: 'Customer',
+      createdAt: new Date(),
+      updatedAt: new Date()
+      }, {
+      // 顧客帳號
+      email: 'test@test.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      isAdmin: false,
       firstName: 'User2',
       lastName: 'Customer',
       createdAt: new Date(),
