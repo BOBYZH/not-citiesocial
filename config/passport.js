1bb72ed2db33b3210e7ee91bd43ca6cecab4692a
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-04-23 00:11:53 +0800

    Fix class error and profile name of FacebookStrategy

--- config/passport.js
+++ config/passport.js
@@ -27,19 +27,20 @@ passport.use(new FacebookStrategy(
     clientID: process.env.FACEBOOK_ID,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK,
-    profileFields: ['email', 'displayName']
+    profileFields: ['email', 'displayName', 'name']
   },
   (_accessToken, _refreshToken, profile, cb) => {
     User.findOne({
       where: { email: profile._json.email }
     }).then(user => {
+      console.log(profile, 'profile')
       if (!user) {
         const randomPassword = Math.random().toString(36).slice(-8)
         bcrypt.genSalt(10, (_err, salt) =>
           bcrypt.hash(randomPassword, salt, (_err, hash) => {
-            const newUser = User({
-              firstName: profile._json.name,
-              lastName: profile.name.familyName,
+            const newUser = new User({
+              firstName: profile._json.first_name,
+              lastName: profile._json.last_name,
               email: profile._json.email,
               password: hash,
               isAdmin: 0
