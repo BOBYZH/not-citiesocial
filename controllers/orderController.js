7c7b8300caa49e8c934624566587b2d21cb2b43f
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-04-26 22:11:07 +0800

    Add nodemailer to send email of order, and tweak codes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,6 +3,21 @@ const Order = db.Order
 const OrderItem = db.OrderItem
 const Cart = db.Cart
 
+// 設定nodemailer，與寄件者服務、帳密
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  secure: true,
+  auth: {
+    type: 'OAuth2',
+    user: process.env.ACCOUNT,
+    clientId: process.env.CLINENTID,
+    clientSecret: process.env.CLINENTSECRET,
+    refreshToken: process.env.REFRESHTOKEN
+  }
+})
+// console.log('processTest', process.env.GMAIL_ACCOUNT, process.env.GMAIL_PASSWORD)
+
 const orderController = {
   getOrders: (req, res) => {
     Order.findAll({ include: 'items' }).then(orders => {
@@ -18,6 +33,7 @@ const orderController = {
         name: req.body.name,
         address: req.body.address,
         phone: req.body.phone,
+        email: req.body.email,
         shippingStatus: req.body.shippingStatus,
         paymentStatus: req.body.paymentStatus,
         amount: req.body.amount
@@ -34,13 +50,40 @@ const orderController = {
             })
           )
         }
-        // 確認所有商品加入訂單後才轉址
-        return Promise.all(results).then(() =>
 
-        // 待優化：刪除已經下訂單的購物車
+        // 信件資訊
+        const mailOptions = {
+          from: process.env.GMAIL_ADDRESS,
+          to: req.body.email,
+          subject: `${order.name}，您的訂單（編號：${order.id}）已成立`,
+          html: `    
+                <div style="display: inline-block; min-width: 300px; background-color: white;">
+                  <h1 style="margin-left: 30px;">您的訂單</h1>
+                  <a href="${process.env.WEBSITE_URL}orders" 
+                  style="margin-top: 5px;">
+                    <h3>編號：${order.id}，請至訂單頁面確認並付款</h3>
+                  </a>
+                  <h3>交易時間：${new Date()}</h3>
+                  <h3>總金額：${order.amount}</h3>
+                </div>
+                `
+        }
+        console.log('Mail, from ', process.env.ADDRESS, ' to ', req.body.email)
+
+        // 確認所有商品加入訂單後才轉址
+        return Promise.all(results).then(() => {
+          // 待優化：刪除已經下訂單的購物車
+
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error)
+            } else {
+              console.log('Email sent: ' + info.response)
+            }
+          })
 
           res.redirect('/orders')
-        )
+        })
       })
     })
   },
