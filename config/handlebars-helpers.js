6db4e24cb241bc2371558854e34ddf95285d769f
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-05-01 01:13:52 +0800

    Add ifNotCond helper

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -5,6 +5,12 @@ module.exports = {
     }
     return options.inverse(this)
   },
+  ifNotCond: function (a, b, options) {
+    if (a !== b) {
+      return options.fn(this)
+    }
+    return options.inverse(this)
+  },
   ifTrue: function (a, options) {
     if (a === true) {
       return options.fn(this)
